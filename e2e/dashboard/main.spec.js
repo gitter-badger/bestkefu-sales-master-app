2a3ede7dace3f3b17e92cc1881be773005cfb0e3
abbish <me@abbish.com>
2015-05-01 12:36:19 +0800

    更新 yemon 构建器 gulp 脚本到新版本，解决 karma 与 webstrom 集成的问题

---
+++ e2e/dashboard/main.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Dashboard', function () {
+
+  describe('main', function () {
+
+    beforeEach(function () {
+      browser.get('http://localhost:3000');
+    });
+
+    it('should view main page title', function() {
+      expect(true).toBe(true);
+    });
+  });
+});

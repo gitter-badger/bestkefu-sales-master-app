0fde7ed0329ada58d9bdb771bed88ca7497ec479
abbish <me@abbish.com>
2015-05-01 21:15:54 +0800

    增加 live template

--- src/app/goods/controllers/create/create.spec.js
+++ src/app/goods/controllers/create/create.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 
-describe('GoodsCreateController', function(){
+describe('GoodsCreateController', function () {
   var scope;
   var vm;
 
   beforeEach(module('sales.goods'));
 
-  beforeEach(inject(function($rootScope, $controller) {
+  beforeEach(inject(function ($rootScope, $controller) {
     scope = $rootScope.$new();
 
     vm = $controller('GoodsCreateController', {
@@ -14,17 +14,17 @@ describe('GoodsCreateController', function(){
     });
   }));
 
-  it('should same to goods schema for form data structure', inject(function(GoodsSchema) {
+  it('should same to goods schema for form data structure', inject(function (GoodsSchema) {
 
     expect(vm.data).toBeDefined();
 
-    _.each(GoodsSchema.structure, function(item, index){
+    _.each(GoodsSchema.structure, function (item, index) {
       expect(vm.data[index]).toBeDefined();
     });
 
   }));
 
-  it('should has error when form data is invalid', inject(function() {
+  it('should has error when form data is invalid', inject(function () {
 
   }));
 });
--- src/app/goods/controllers/view/view.spec.js
+++ src/app/goods/controllers/view/view.spec.js
@@ -1,21 +1,35 @@
 'use strict';
 
 describe('GoodsViewController', function () {
-    var scope;
-    var vm;
+  var scope;
+  var vm;
+  var rs;
 
-    beforeEach(module('sales.goods'));
+  beforeEach(module('sales.goods'));
 
-    beforeEach(inject(function ($rootScope, $controller) {
-        scope = $rootScope.$new();
+  beforeEach(inject(function ($rootScope, $controller) {
+    scope = $rootScope.$new();
 
-        vm = $controller('GoodsViewController', {
-            $scope: scope
-        });
-    }));
+    vm = $controller('GoodsViewController', {
+      $scope: scope
+    });
 
-    it('should defined vm', inject(function () {
+    _.extend(vm, {
+      abbish: function(){
+        return true;
+      }
+    });
 
-        expect(true).toBe(true);
-    }));
+    spyOn(vm, 'abbish').and.callThrough();
+
+    rs = vm.abbish();
+  }));
+
+  it('should have called abbish method', inject(function () {
+    expect(vm.abbish).toHaveBeenCalled();
+  }));
+
+  it('should return true when call abbish method', inject(function() {
+    expect(rs).toBe(true);
+  }));
 });

29e3d9432e9993066a531e9c3a5a0152594d26fe
abbish <me@abbish.com>
2015-05-02 22:10:22 +0800

    还原

--- e2e/dashboard/main.spec.js
+++ e2e/dashboard/main.spec.js
@@ -20,7 +20,7 @@
         });
 
         element(by.css('a[href="#/order"]')).getText().then(function(text) {
-          expect(text).toBe('Goto Bii');
+          expect(text).toBe('Goto Order');
         });
       });
     });
